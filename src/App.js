287546bfaf3884e19d95bb78a30528a90258e796
Dubey Ravi Vinod <dubeyravivinod@gmail.com>
2023-02-25 23:17:33 +0530

    Mapping Array Manually

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Matrix from './Components/Matrix'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{
+                  fontFamily: "monospace",
+                  fontSize: "45px",
+                  color: "red"
+                }}>30 Days of React</h1>
+      <h2 style={{
+                  fontFamily: "monospace",
+                  fontSize: "30px"
+                }}>Number Generator</h2>
+      
+      <Matrix />
     </div>
   );
 }
---
+++ src/Components/Matrix.js
@@ -0,0 +1,298 @@
+import './Matrix.css'
+
+const MatrixArray = () => {
+    return (
+        <div className="MainDiv">
+            <div className="Matrix">
+                <div
+                    style={
+                        {
+                            backgroundColor: "#21bf73"
+                        }
+                    }
+                >0</div>
+            </div>
+            <div className="Matrix">
+                <div
+                    style={
+                        {
+                            backgroundColor: "#fddb3a"
+                        }
+                    }
+                >1</div>
+            </div>
+            <div className="Matrix">
+                <div
+                    style={
+                        {
+                            backgroundColor: "#fd5e53"
+                        }
+                    }
+                >2</div>
+            </div>
+            <div className="Matrix">
+                <div
+                    style={
+                        {
+                            backgroundColor: "#fd5e53"
+                        }
+                    }
+                >3</div>
+            </div>
+            <div className="Matrix">
+                <div
+                    style={
+                        {
+                            backgroundColor: "#21bf73"
+                        }
+                    }
+                >4</div>
+            </div>
+            <div className="Matrix">
+                <div
+                    style={
+                        {
+                            backgroundColor: "#fd5e53"
+                        }
+                    }
+                >5</div>
+            </div>
+            <div className="Matrix">
+                <div
+                    style={
+                        {
+                            backgroundColor: "#21bf73"
+                        }
+                    }
+                >6</div>
+            </div>
+            <div className="Matrix">
+                <div
+                    style={
+                        {
+                            backgroundColor: "#fd5e53"
+                        }
+                    }
+                >7</div>
+            </div>
+            <div className="Matrix">
+                <div
+                    style={
+                        {
+                            backgroundColor: "#21bf73"
+                        }
+                    }
+                >8</div>
+            </div>
+            <div className="Matrix">
+                <div
+                    style={
+                        {
+                            backgroundColor: "#fddb3a"
+                        }
+                    }
+                >9</div>
+            </div>
+            <div className="Matrix">
+                <div
+                    style={
+                        {
+                            backgroundColor: "#21bf73"
+                        }
+                    }
+                >10</div>
+            </div>
+            <div className="Matrix">
+                <div
+                    style={
+                        {
+                            backgroundColor: "#fd5e53"
+                        }
+                    }
+                >11</div>
+            </div>
+            <div className="Matrix">
+                <div
+                    style={
+                        {
+                            backgroundColor: "#21bf73"
+                        }
+                    }
+                >12</div>
+            </div>
+            <div className="Matrix">
+                <div
+                    style={
+                        {
+                            backgroundColor: "#fd5e53"
+                        }
+                    }
+                >13</div>
+            </div>
+            <div className="Matrix">
+                <div
+                    style={
+                        {
+                            backgroundColor: "#21bf73"
+                        }
+                    }
+                >14</div>
+            </div>
+            <div className="Matrix">
+                <div
+                    style={
+                        {
+                            backgroundColor: "#fddb3a"
+                        }
+                    }
+                >15</div>
+            </div>
+            <div className="Matrix">
+                <div
+                    style={
+                        {
+                            backgroundColor: "#21bf73"
+                        }
+                    }
+                >16</div>
+            </div>
+            <div className="Matrix">
+                <div
+                    style={
+                        {
+                            backgroundColor: "#fd5e53"
+                        }
+                    }
+                >17</div>
+            </div>
+            <div className="Matrix">
+                <div
+                    style={
+                        {
+                            backgroundColor: "#21bf73"
+                        }
+                    }
+                >18</div>
+            </div>
+            <div className="Matrix">
+                <div
+                    style={
+                        {
+                            backgroundColor: "#fd5e53"
+                        }
+                    }
+                >19</div>
+            </div>
+            <div className="Matrix">
+                <div
+                    style={
+                        {
+                            backgroundColor: "#21bf73"
+                        }
+                    }
+                >20</div>
+            </div>
+            <div className="Matrix">
+                <div
+                    style={
+                        {
+                            backgroundColor: "#fddb3a"
+                        }
+                    }
+                >21</div>
+            </div>
+            <div className="Matrix">
+                <div
+                    style={
+                        {
+                            backgroundColor: "#21bf73"
+                        }
+                    }
+                >22</div>
+            </div>
+            <div className="Matrix">
+                <div
+                    style={
+                        {
+                            backgroundColor: "#fd5e53"
+                        }
+                    }
+                >23</div>
+            </div>
+            <div className="Matrix">
+                <div
+                    style={
+                        {
+                            backgroundColor: "#21bf73"
+                        }
+                    }
+                >24</div>
+            </div>
+            <div className="Matrix">
+                <div
+                    style={
+                        {
+                            backgroundColor: "#fddb3a"
+                        }
+                    }
+                >25</div>
+            </div>
+            <div className="Matrix">
+                <div
+                    style={
+                        {
+                            backgroundColor: "#21bf73"
+                        }
+                    }
+                >26</div>
+            </div>
+            <div className="Matrix">
+                <div
+                    style={
+                        {
+                            backgroundColor: "#fddb3a"
+                        }
+                    }
+                >27</div>
+            </div>
+            <div className="Matrix">
+                <div
+                    style={
+                        {
+                            backgroundColor: "#21bf73"
+                        }
+                    }
+                >28</div>
+            </div>
+            <div className="Matrix">
+                <div
+                    style={
+                        {
+                            backgroundColor: "#fd5e53"
+                        }
+                    }
+                >29</div>
+            </div>
+            <div className="Matrix">
+                <div
+                    style={
+                        {
+                            backgroundColor: "#21bf73"
+                        }
+                    }
+                >30</div>
+            </div>
+            <div className="Matrix">
+                <div
+                    style={
+                        {
+                            backgroundColor: "#fd5e53"
+                        }
+                    }
+                >31</div>
+            </div>
+        </div>
+    );
+}
+
+export default MatrixArray;
